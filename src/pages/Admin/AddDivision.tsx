5328603511e0dab704126832f114ea7dac877317
Your Name <khalidsaifullaha3@gmail.com>
2025-09-13 14:19:36 +0600

    add image preview functionality and manage form data handling

--- src/pages/Admin/AddDivision.tsx
+++ src/pages/Admin/AddDivision.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { AddDivisionModal } from "@/components/modules/Admin/Division/AddDivisionModal";
 
 const AddDivision = () => {
-  return <div>AddDivision</div>;
+  return (
+    <div>
+      <h1>Add Description</h1>
+      <AddDivisionModal />
+    </div>
+  );
 };
 
 export default AddDivision;
--- src/routes/adminSidebarItems.tsx
+++ src/routes/adminSidebarItems.tsx
@@ -1,3 +1,4 @@
+import AddDivision from "@/pages/Admin/AddDivision";
 import AddTour from "@/pages/Admin/AddTour";
 import AddTourType from "@/pages/Admin/AddTourType";
 import { lazy } from "react";
@@ -22,14 +23,20 @@ const adminSidebarItems = [
 
     items: [
       {
-        title: "Add Tour",
-        url: "/admin/add-tour",
-        component: AddTour,
-      },
-      {
         title: "Add Tour Type",
         url: "/admin/add-tour-type",
         component: AddTourType,
+      },
+
+      {
+        title: "Add Division",
+        url: "/admin/add-division",
+        component: AddDivision,
+      },
+      {
+        title: "Add Tour",
+        url: "/admin/add-tour",
+        component: AddTour,
       },
     ],
   },
